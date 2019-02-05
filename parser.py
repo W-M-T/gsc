1668cfb25ecba08cf4ba815496ff06edde34bf4d
W-M-T <wgitcommit@gmail.com>
2019-02-05 18:03:15 +0100

    Lexer strategy

--- parser.py
+++ parser.py
@@ -38,6 +38,23 @@ TODO
 Verplaats datastructuren naar een apart bestand
 '''
 
+'''
+Lexer heeft string flag
+single line comment flag
+multiline comment flag
+type-of-waarde-context flag (voor -> en , en []), geactiveerd via ::
+
+consumeert keywords als deze niet door iets alfabetisch gevolgd worden
+
+=-teken wordt gewoon gelext als assignment -> dus geen geldige operatornaam
+eindigen in comment-mode is error
+multiline comment mode sluiten zonder flag is error -> */ geen geldige operatornaam
+Vinden van commentsymbolen buiten string mode begint altijd comment
+oftewel //* en /* geen geldige operatornamen
+
+
+'''
+
 
 class Token():
     def __init__(self, pos, typ, val):
