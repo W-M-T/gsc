9a9b2165e4726ff4b3dc9883b1931a7c7e38d7fc
W-M-T <wgitcommit@gmail.com>
2019-02-05 12:30:37 +0100

    Grammar update + parser start

--- parser.py
+++ parser.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 
+class Position():
+    def __init__(self, line, col)
+        self.line = line
+        self.col  = col
+
 class Node():
-    def __init__(self, x, children):
-        self.x = x
-        self.children = children
+    def __init__(self, pos, token, children):
+        self.pos      = pos
+        self.token    = token
+        self.children = list(children)
