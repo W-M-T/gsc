2fd565331142e31956f61ca3e7beaba550146969
W-M-T <wgitcommit@gmail.com>
2019-02-06 16:50:40 +0100

    Work on lexer and parser

--- lexer.py
+++ lexer.py
@@ -2,19 +2,88 @@
 
 import time
 import sys
+import re
+
+from util import TOKEN, Token, Position
+
+# Constants
+
+KEYWORDS = {
+    "var"    : TOKEN.VAR,
+    "if"     : TOKEN.IF,
+    "elif"   : TOKEN.ELIF,
+    "else"   : TOKEN.ELSE,
+    "while"  : TOKEN.WHILE,
+    "for"    : TOKEN.FOR,
+    "return" : TOKEN.RETURN,
+    "prefix" : TOKEN.PREFIX,
+    "infixl" : TOKEN.INFIXL,
+    "infixr" : TOKEN.INFIXR,
+    "type"   : TOKEN.TYPESYN
+}
+VALUES = {
+    "True"  : TOKEN.BOOL,
+    "False" : TOKEN.BOOL,
+    "[]"    : TOKEN.EMPTY_LIST
+}
+TYPES = {
+    "Void" : TOKEN.TYPE_VOID,
+    "Int"  : TOKEN.TYPE_INT,
+    "Bool" : TOKEN.TYPE_BOOL,
+    "Char" : TOKEN.TYPE_CHAR
+}
+ACCESSORS = {
+    ".hd"  : TOKEN.ACCESSOR,
+    ".tl"  : TOKEN.ACCESSOR,
+    ".fst" : TOKEN.ACCESSOR,
+    ".snd" : TOKEN.ACCESSOR,
+}
+
+# Regexes (need to be checked)
+
+REG_ID  = re.compile("[a-z][a-z_]*")
+REG_OP  = re.compile("[!#$%&*+/<=>?@\\^|:,~-]+")
+REG_INT = re.compile("\\d+")
+# regex for string and char? Or handle differently?
 
 
 def tokenize(filename):
+    FLAG_STRING         = False
+    FLAG_CHAR           = False
+    FLAG_SINGLE_COMMENT = False # Not necessary, just discard current line from here
+    FLAG_MULTI_COMMENT  = False
+    FLAG_TYPE_CONTEXT   = False
+
+    pos = Position() # Is it not a problem if this were to get passed by reference?
+
+    yield(Token(pos, TOKEN.TYPE_VOID, "Hello world"))
+
     with open(filename, "r") as infile:
-        while True:
-            c = infile.read(1)
-            if not c:
-                break
-            yield(c)
+        curdata = ""
+        for line in infile:
+            curdata = line
+            while len(curdata) > 0:
+                curdata = curdata.lstrip()
+
+                # If not in a string or char:
+
+                    # Test for comment
+
+                    # Test for keyword tokens
+
+                    # Test for string start
+
+                    # (TODO Distinguish between type / assignment context in logic)
+
+
+                return
+
+
 
 
 if __name__ == "__main__":
     for t in tokenize("./example programs/p1_example.spl"):
-        print(t, end="")
+        print(t.typ, end=" ")
         sys.stdout.flush()
-        time.sleep(0.05)
+        time.sleep(0.1)
+    print("end")
--- parser.py
+++ parser.py
@@ -1,33 +1,6 @@
 #!/usr/bin/env python3
 
-TOKEN_VAR
-TOKEN_IF
-TOKEN_ELIF
-TOKEN_ELSE
-TOKEN_WHILE
-TOKEN_FOR
-TOKEN_RETURN
-TOKEN_PREFIX
-TOKEN_INFIXL
-TOKEN_INFIXR
-TOKEN_TYPESYN
-TOKEN_BOOL
-TOKEN_EMPTY_LIST
-TOKEN_CHAR
-TOKEN_INT
-TOKEN_TYPE_VOID
-TOKEN_TYPE_INT
-TOKEN_TYPE_BOOL
-TOKEN_TYPE_CHAR
-TOKEN_PAR_OPEN
-TOKEN_PAR_CLOSE
-TOKEN_CURL_OPEN
-TOKEN_CURL_CLOSE
-TOKEN_BRACK_OPEN
-TOKEN_BRACK_CLOSE
-TOKEN_ACCESSOR
-TOKEN_IDENTIFIER
-TOKEN_OP_IDENTIFIER
+
 
 
 '''
@@ -56,20 +29,3 @@ oftewel //* en /* geen geldige operatornamen
 '''
 
 
-class Token():
-    def __init__(self, pos, typ, val):
-        self.pos = pos
-        self.typ = val
-        self.val = val
-
-
-class Position():
-    def __init__(self, line, col)
-        self.line = line
-        self.col  = col
-
-
-class Node():
-    def __init__(self, pos, token, children):
-        self.token    = token
-        self.children = list(children)
