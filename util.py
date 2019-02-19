4867063f33ebea445c450df1f0b72af579deed0e
W-M-T <wgitcommit@gmail.com>
2019-02-19 16:10:27 +0100

    Update token types + add debug pretty printer

--- util.py
+++ util.py
@@ -18,23 +18,57 @@ class TOKEN(IntEnum): # auto() for python >3.5
     EMPTY_LIST    = 13
     CHAR          = 14
     INT           = 15
-    TYPE_VOID     = 16
-    TYPE_INT      = 17
-    TYPE_BOOL     = 18
-    TYPE_CHAR     = 19
-    PAR_OPEN      = 20
-    PAR_CLOSE     = 21
-    CURL_OPEN     = 22
-    CURL_CLOSE    = 23
-    BRACK_OPEN    = 24
-    BRACK_CLOSE   = 25
-    SEMICOLON     = 26
-    TYPE_SIG      = 27
-    ASSIGNMENT    = 28
-    ARROW         = 29
-    TYPE_COMMA    = 30
-    ACCESSOR      = 31
+    TYPE_KEYWORD  = 16
+    PAR_OPEN      = 17
+    PAR_CLOSE     = 18
+    CURL_OPEN     = 19
+    CURL_CLOSE    = 20
+    BRACK_OPEN    = 21
+    BRACK_CLOSE   = 22
+    SEMICOLON     = 23
+    TYPE_SIG      = 24
+    ASSIGNMENT    = 25
+    ARROW         = 26
+    TYPE_COMMA    = 27
+    ACCESSOR      = 28
+    IDENTIFIER    = 29
+    OP_IDENTIFIER = 30
+    STRING        = 31
 
+
+PRETTY_TOKEN = {
+    TOKEN.VAR           : (lambda x: "var"),
+    TOKEN.IF            : (lambda x: "if"),
+    TOKEN.ELIF          : (lambda x: "elif"),
+    TOKEN.ELSE          : (lambda x: "else"),
+    TOKEN.WHILE         : (lambda x: "while"),
+    TOKEN.FOR           : (lambda x: "for"),
+    TOKEN.RETURN        : (lambda x: "return"),
+    TOKEN.PREFIX        : (lambda x: "prefix"),
+    TOKEN.INFIXL        : (lambda x: "infixl"),
+    TOKEN.INFIXR        : (lambda x: "infixr"),
+    TOKEN.TYPESYN       : (lambda x: "type"),
+    TOKEN.BOOL          : (lambda x: x.val),
+    TOKEN.EMPTY_LIST    : (lambda x: "[]"),
+    TOKEN.CHAR          : (lambda x: "(CHAR)'{}'".format(x.val)),
+    TOKEN.INT           : (lambda x: x.val),
+    TOKEN.TYPE_KEYWORD  : (lambda x: x.val),
+    TOKEN.PAR_OPEN      : (lambda x: "("),
+    TOKEN.PAR_CLOSE     : (lambda x: ")"),
+    TOKEN.CURL_OPEN     : (lambda x: "{"),
+    TOKEN.CURL_CLOSE    : (lambda x: "}"),
+    TOKEN.BRACK_OPEN    : (lambda x: "["),
+    TOKEN.BRACK_CLOSE   : (lambda x: "]"),
+    TOKEN.SEMICOLON     : (lambda x: ";"),
+    TOKEN.TYPE_SIG      : (lambda x: "::"),
+    TOKEN.ASSIGNMENT    : (lambda x: "="),
+    TOKEN.ARROW         : (lambda x: "(TYPE)->"),
+    TOKEN.TYPE_COMMA    : (lambda x: "(TYPE),"),
+    TOKEN.ACCESSOR      : (lambda x: x.val),
+    TOKEN.IDENTIFIER    : (lambda x: "(ID){}".format(x.val)),
+    TOKEN.OP_IDENTIFIER : (lambda x: "(OP){}".format(x.val)),
+    TOKEN.STRING        : (lambda x: "(STRING)\"{}\"".format(x.val)),
+}
 
 class Token():
     def __init__(self, pos, typ, val):
@@ -48,6 +82,9 @@ class Token():
     def mini_str(self):
         return "<{}>".format(self.typ.name)
 
+    def pretty(self):
+        return PRETTY_TOKEN[self.typ](self)
+
 
 class Position():
     def __init__(self, line = 1, col = 1):
@@ -56,6 +93,10 @@ class Position():
 
     def __repr__(self):
         return "line {} :col {}".format(self.line, self.col)
+
+    def copy(self):
+        return Position(self.line, self.col)
+
 
 class Node():
     def __init__(self, pos, token, children):
