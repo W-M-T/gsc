dd68b529b69db1dc4d9eb97087d9622bd6aef320
W-M-T <wgitcommit@gmail.com>
2019-03-04 15:55:28 +0100

    Start of parser

---
+++ miniparsec.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def many1:
+    pass
+
+def many:
+    pass
+
+def optional:
+    pass
+
+
+class Parser():
+
+    def parse(self, tokenlist):
+        pass
+
+    
--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from util import pointToPosition, Position
+from util import pointToPosition, Position, TOKEN, Node
 
 '''
 Zaken als +, -, :, / etc zijn built-in methodes
@@ -18,6 +18,34 @@ Vinden van commentsymbolen buiten string mode begint altijd comment
 oftewel //* en /* geen geldige operatornamen
 
 
+'''
+
+'''
+Top-down parser
+Leftmost derivation
+Non-ambigue grammatica
+Geen left-recursion
+LL(1) (1 token lookahead)
+
+Eindige recursie-diepte dmv stack.
+
+
+De taal is zo gespecificeerd dat de eerstvolgende token altijd maar bij 1 regel zou moeten passen.
+Zo niet geef een !!COMPILER ERROR!! want dan zijn dingen goed fout.
+Een regel/parser specificeert een verwachte token + een filterfunctie (i.e. TOKEN.OP_ID met value == "="). Default is lambda _: True
+Afhankelijk van je positie in de parser heb je meerdere mogelijke opties van regels/parsers.
+Als een nonterminal een optie is beschouw je deze gewoon als een lijst van al diens alternatieven naar hetzelfde niveau getild als de huidige.
+Houd een soort stack bij van hoe je regels afdaalt. Als het een optie is om de huidige te beeindigen (maar nog niet verplicht) bechouw je ook de regel erboven.
+
+
+Als zeker is dat de huidige regel afgesloten wordt moet een waarde ge-emit worden. Deze wordt in de regel erboven gebruikt voor het emitten van diens waarde.
+
+Second pass:
+Fix de expressie-associativiteit en bindingssterkte aan de hand van de operator-definities
+
+
+Accessors worden vertaald naar functiecalls met builtin functies.
+Moeten ook al een soort symbol-table maken van operator-definities
 '''
 
 
