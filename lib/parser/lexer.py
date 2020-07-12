b1ea61713e5a24a7fb1f74f5bff75a9f2816a93f
W-M-T <wgitcommit@gmail.com>
2020-07-12 14:40:28 +0200

    Fix string lex

--- lib/parser/lexer.py
+++ lib/parser/lexer.py
@@ -153,8 +153,9 @@ def prefix_val_literal(string): # TODO improve this code
     tempmatch = REG_STR.match(string)
     if tempmatch:
         found_id = tempmatch.group(0)
+        conv_str = codecs.getdecoder("unicode_escape")(found_id)[0][1:-1]
         rest = string[len(found_id):]
-        return (True, rest, TOKEN.STRING, found_id) 
+        return (True, rest, TOKEN.STRING, conv_str) 
     tempmatch = REG_CHR.match(string)
     if tempmatch:
         found_id = tempmatch.group(0)
